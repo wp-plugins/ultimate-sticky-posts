93ba0545e05ee1961d63f78d80b018ad3532c2d2
Pieter Ferrreira <Pieter Ferrreira@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-14 11:12:14 +0000

    tagging version 2.2.3
    git-svn-id: https://plugins.svn.wordpress.org/ultimate-sticky-posts/tags/2.2.3@1220805 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sticky-posts.php
+++ sticky-posts.php
@@ -3,11 +3,10 @@
 Plugin Name: Ultimate Sticky Posts Widget
 Description: Adds a widget that shows your posts the way you want.
 Author: Pieter Ferreira
-Version: 1.0.1
+Version: 1.2.3
 License: GPLv2
 */
 
-// Creating the widget 
 add_action( 'wp_enqueue_scripts', 'sticky_posts_widget_styles' );
 function sticky_posts_widget_styles() {
 	wp_register_style( 'sticky-posts', plugins_url( 'sticky-posts/sticky.css' ) );
@@ -18,11 +17,11 @@ class bsp_widget extends WP_Widget {
 
 function __construct() {
 parent::__construct(
-// Base ID of your widget
+// Base ID of the widget
 'bsp_widget', 
 
-// Widget name will appear in UI
-__('Sticky Posts Widget', 'bsp_widget_domain'), 
+// Widget name that appears in UI
+__('Ulitmate Sticky Posts Widget', 'bsp_widget_domain'), 
 
 // Widget description
 array( 'description' => __( 'Display your posts the way you want to!, sticky or not.', 'bsp_widget_domain' ), ) 
@@ -30,12 +29,9 @@ array( 'description' => __( 'Display your posts the way you want to!, sticky or 
 }
 
 // Creating widget front-end
-// This is where the action happens
 public function widget( $args, $instance ) {
 
 
-// before and after widget arguments are defined by themes
-echo $args['before_widget'];
 
 if ( ! empty( $title ) )
 echo $args['before_title'] . $title . $args['after_title'];
@@ -47,7 +43,7 @@ echo $args['before_title'] . $title . $args['after_title'];
     $orderby = $instance['orderby'];
     echo $before_widget;
 
-      // Sticky posts
+      // Ultimate Sticky posts Query
       
       if ($sticky == 'only') {
         $sticky_query = $args = array( 
@@ -84,7 +80,7 @@ echo $args['before_title'] . $title . $args['after_title'];
 			<?php
 			if ( $title ) {
        		echo $before_title;
-        		echo "<h3>" . $title . "</h3>";
+        		echo "<h2>" . $title . "</h2>";
         	echo $after_title;
       		}
    				$featured = new WP_Query($args); 
@@ -99,9 +95,7 @@ echo $args['before_title'] . $title . $args['after_title'];
 					</div>
 				<?php endif; ?>
 				<?php if ( isset( $instance['show_title'] ) ) : ?>
-					<div class="featured_bottom">
-						<div class="bsp_title"><h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3></div>
-					</div>
+					<div class="bsp_title"><h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3></div>
 				<?php endif; ?>
 				
 				<?php if ( isset( $instance['show_excerpt'] ) ) : ?>
@@ -253,8 +247,6 @@ $show_thumbnail = $instance['show_thumbnail'];
             show_thumbnail.click(function(){
               thumb_size_wrap.toggle('fast');
             });
-
-            // Show or hide custom template field
 
           });
 
